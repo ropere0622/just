96badded88005e2c04484667a81ce364648f02ab
ropere0622 <ropere0622@gmail.com>
2017-03-27 14:50:29 +0800

    update

---
+++ js/basic.js
@@ -0,0 +1,9 @@
+function tbtn1(){ location.href='intro.html';}
+function tbtn2(){ location.href='college.html';}
+function tbtn3(){ location.href='admissions.html';}
+function tbtn4(){ location.href='recruit.html';}
+function tbtn5(){ location.href='video.html';}
+function tbtn6(){ location.href='osrecruit.html';} 
+function tbtn7(){ location.href='board.html';}
+function tbtn8(){ location.href='contact.html';}
+
